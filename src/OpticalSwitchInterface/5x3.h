67f63d277492bf98b8d353fa15f69faa63a0465c
Peter Polidoro <peterpolidoro@gmail.com>
2018-08-12 12:54:07 -0400

    polidorop@janelia.hhmi.org -> peterpolidoro@gmail.com

--- src/OpticalSwitchInterface/5x3.h
+++ src/OpticalSwitchInterface/5x3.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro polidorop@janelia.hhmi.org
+// Peter Polidoro peterpolidoro@gmail.com
 // ----------------------------------------------------------------------------
 #ifndef OPTICAL_SWITCH_INTERFACE_5X3_CONSTANTS_H
 #define OPTICAL_SWITCH_INTERFACE_5X3_CONSTANTS_H
